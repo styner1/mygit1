8600fbc392ce64ea66184acc6a07b82ee6f5e14f
zeus <karl0@naver.com>
2016-09-02 16:58:13 +0900

    ffff

---
+++ Git_Zeus_1104/src/com/bitacademy/git/HelloGit.java
@@ -0,0 +1,14 @@
+package com.bitacademy.git;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하신가 Git");
+		System.out.println("부두교에 너무 심취하진 말게");
+		//볼진님은 살아계신다!
+		//부두교의 가르침을 받게~
+		// 불의 세례를 받아라!
+		System.out.println("잘 가시게 친구1!");
+		//kkkkk
+	}
+}
